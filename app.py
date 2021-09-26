043e92f90b45e8f86d08fde25a4de92a1c43da20
stephen rigney <Stephenrigneysmr@gmail.com>
2021-09-26 19:45:11 -0400

    final commit

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ import numpy as np
 import flask
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, session
 from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
+from datetime import datetime
 
 engine = create_engine("sqlite:///hawaii.sqlite")
 Base = automap_base()
@@ -21,6 +22,8 @@ def welcome():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/(enter start date YYYY-MM-DD)<br/>"
+        f"/api/v1.0/(enter start date YYYY-MM-DD)/ (enter end date YYYY-MM-DD)<br/>"
     )
 
 @app.route('/api/v1.0/precipitation')
@@ -49,10 +52,48 @@ def stations():
 def tobs():
     session = Session(engine)
     tobs_results = session.query(measurement.date,measurement.tobs).filter(measurement.station == 'USC00519281').filter(measurement.date>'2016-08-23').\
-group_by(measurement.date).order_by(measurement.date.desc()).all()
+        group_by(measurement.date).order_by(measurement.date.desc()).all()
     session.close()
     alltobs = list(np.ravel(tobs_results))
     return jsonify(alltobs)
+
+@app.route('/api/v1.0/<start>')
+def startdate(start):
+    strt = datetime.strptime(start, "%Y-%m-%d").date()
+    session = Session(engine)
+    mostactivemin = session.query(func. min(measurement.tobs)).\
+        filter((measurement.date >= strt)&(measurement.\
+            station == 'USC00519281')).all()
+    mostactivemax = session.query(func. max(measurement.tobs)).\
+        filter((measurement.date >= strt)&(measurement.\
+            station == 'USC00519281')).all()
+    mostactiveavg = session.query(func. avg(measurement.tobs)).\
+        filter((measurement.date >= strt)&(measurement.\
+            station == 'USC00519281')).all()
+    session.close()
+    minmaxavg = mostactivemin, mostactivemax, mostactiveavg
+    allstartdate = list(np.ravel(minmaxavg))
+    return jsonify(allstartdate)
+
+@app.route('/api/v1.0/<start>/<end>')
+def startenddate(start,end):
+    strt = datetime.strptime(start, "%Y-%m-%d").date()
+    ed = datetime.strptime(end, "%Y-%m-%d").date()
+    session = Session(engine)
+    mostactivemin = session.query(func. min(measurement.tobs)).\
+        filter((measurement.date >= strt)&(measurement.date <= ed)&\
+            (measurement.station == 'USC00519281')).all()
+    mostactivemax = session.query(func. max(measurement.tobs)).\
+        filter((measurement.date >= strt)&(measurement.date <= ed)&\
+            (measurement.station == 'USC00519281')).all()
+    mostactiveavg = session.query(func. avg(measurement.tobs)).\
+        filter((measurement.date >= strt)&(measurement.date <= ed)&\
+            (measurement.station == 'USC00519281')).all()
+    session.close()
+    minmaxavg = mostactivemin, mostactivemax, mostactiveavg
+    allstartend = list(np.ravel(minmaxavg))
+    return jsonify(allstartend)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
